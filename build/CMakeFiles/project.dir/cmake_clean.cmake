e26415012d24bf8f98efd2c620e720759e6ad018
jmh1406170116 <97400575+jmh1406170116@users.noreply.github.com>
2025-03-14 12:06:06 +0000

    Add files via upload

---
+++ build/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/home/admin/Desktop/work_space/Project/bin/project"
+  "/home/admin/Desktop/work_space/Project/bin/project.pdb"
+  "CMakeFiles/project.dir/src/SerialPort.cpp.o"
+  "CMakeFiles/project.dir/src/SerialPort.cpp.o.d"
+  "CMakeFiles/project.dir/src/main.cpp.o"
+  "CMakeFiles/project.dir/src/main.cpp.o.d"
+  "CMakeFiles/project.dir/src/qmqtt_client.cpp.o"
+  "CMakeFiles/project.dir/src/qmqtt_client.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
