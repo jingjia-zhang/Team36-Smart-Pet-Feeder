5436d733a5f38df55a753c166e8ce72a365a2d8c
HongjiZhang888 <zbzhjhappy@163.com>
2025-04-17 23:40:22 +0100

    Add files via upload

---
+++ paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/Base64.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/Base64.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/Clients.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/Clients.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/Heap.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/Heap.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/LinkedList.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/LinkedList.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/Log.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/Log.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/MQTTPacket.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/MQTTPacket.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/MQTTPacketOut.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/MQTTPacketOut.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/MQTTPersistence.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/MQTTPersistence.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/MQTTPersistenceDefault.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/MQTTPersistenceDefault.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/MQTTProperties.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/MQTTProperties.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/MQTTProtocolClient.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/MQTTProtocolClient.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/MQTTProtocolOut.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/MQTTProtocolOut.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/MQTTReasonCodes.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/MQTTReasonCodes.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/MQTTTime.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/MQTTTime.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/Messages.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/Messages.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/Proxy.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/Proxy.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/SHA1.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/SHA1.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/Socket.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/Socket.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/SocketBuffer.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/SocketBuffer.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/StackTrace.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/StackTrace.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/Thread.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/Thread.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/Tree.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/Tree.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/WebSocket.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/WebSocket.c.o"
+  "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/src/utf-8.c" "/home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build/src/CMakeFiles/common_obj.dir/utf-8.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "PAHO_MQTT_EXPORTS=1"
+  "_GNU_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
