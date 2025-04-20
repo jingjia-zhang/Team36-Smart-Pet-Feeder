5de84fea2085ccc63005cc8934503d44c1d7fae1
RunzeZhang121 <zrz1201@outlook.com>
2025-04-20 11:19:20 +0800

    remove paho.mqtt.c-1.3.14

--- paho.mqtt.c-1.3.14/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/admin/Desktop/work_space/paho.mqtt.c-1.3.14
-# Build directory: /home/admin/Desktop/work_space/paho.mqtt.c-1.3.14/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
