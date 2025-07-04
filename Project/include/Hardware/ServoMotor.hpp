d10347761af3cfde43e8e9565502bdc30eaa01f6
RunzeZhang121 <zrz1201@outlook.com>
2025-07-04 16:33:16 +0100

    servomotor

---
+++ Project/include/Hardware/ServoMotor.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "Hardware/GPIOChip.hpp"
+#include <memory>
+#include <chrono>
+
+class IServoMotor {
+public:
+    virtual void setAngle(int angle) = 0; // 0-180 degrees
+    virtual ~IServoMotor() = default;
+};
+
+class ServoMotor : public IServoMotor {
+public:
+    ServoMotor(std::shared_ptr<GPIOChip> gpio, int pin);
+    void setAngle(int angle) override;
+    
+private:
+    std::shared_ptr<GPIOChip> gpio_;
+    int pin_;
+    int currentAngle_;
+    
+    void generatePulse(int widthUs);
+};
